57bf9df0e151bac91e2c518bf6030d4df6f265b6
gabor <gabor.racz@klarna.com>
2018-05-26 11:16:37 +0200

    Test scheduling

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: ./release-tasks.sh
+schedule: python schedule.py
 web: gunicorn project.wsgi --name im-ok-core-service --workers 3 --access-logfile "-" --error-logfile "-"
---
+++ schedule.py
@@ -0,0 +1,15 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from django.core import management
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=24)
+def scheduled_job():
+    print('Run generate_call_list')
+    management.call_command('generate_call_list')
+    print('Run reset_should_be_contacted')
+    management.call_command('reset_should_be_contacted')
+
+
+sched.start()
