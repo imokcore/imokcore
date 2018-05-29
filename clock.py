2e61a107c950bf1c6db92dfc8233634a889f9652
gabor <gabor.racz@klarna.com>
2018-05-29 10:31:58 +0200

    Update clock

--- clock.py
+++ clock.py
@@ -6,7 +6,10 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    print('This job is run every three minutes.')
+    print('Run generate_call_list')
+    management.call_command('generate_call_list')
+    print('Run reset_should_be_contacted')
+    management.call_command('reset_should_be_contacted')
 
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=22)
