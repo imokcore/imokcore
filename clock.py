2576425e7d89aa07a49ed4001af0606148670d04
gabor <gabor.racz@klarna.com>
2018-05-29 22:32:40 +0200

    Fix clock

--- clock.py
+++ clock.py
@@ -1,18 +1,13 @@
+import os
+
+import django
 from apscheduler.schedulers.blocking import BlockingScheduler
-from django.conf import settings
 from django.core import management
 
-settings.configure()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings.prod'
+django.setup()
 
 sched = BlockingScheduler()
-
-
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('Run generate_call_list')
-    management.call_command('generate_call_list')
-    print('Run reset_should_be_contacted')
-    management.call_command('reset_should_be_contacted')
 
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=22)
