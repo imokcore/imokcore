3fa77ba9cd5f19dfd4319879d881f6c45940cc95
gabor <gabor.racz@klarna.com>
2018-05-30 08:17:54 +0200

    test clock

--- clock.py
+++ clock.py
@@ -10,6 +10,14 @@ django.setup()
 sched = BlockingScheduler()
 
 
+@sched.scheduled_job('interval', minutes=0.1)
+def timed_job():
+    print('Run generate_call_list')
+    management.call_command('generate_call_list')
+    print('Run reset_should_be_contacted')
+    management.call_command('reset_should_be_contacted')
+
+
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=22)
 def scheduled_job():
     print('Run generate_call_list')
